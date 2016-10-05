97315bc25f1401d653494fb6198e48e5de5f0d3f
thunderenlight <thunderenlight@gmail.com>
2016-10-05 17:58:18 -0500

    Initializing the public activity views

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,12 @@
 class Story < ActiveRecord::Base
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user ? controller.current_user : nil },
+  		  title: proc { |controller, model| model.title }
+
   belongs_to :user
 
   validates :title, presence: true
   validates :body, presence: true
 
-  include PublicActivity::Model
-  tracked
 end
