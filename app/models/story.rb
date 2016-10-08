7aaf0110215e44308d04842301daec4681481e00
thunderenlight <thunderenlight@gmail.com>
2016-10-08 18:26:01 -0500

    customizing the activity model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,8 @@
 class Story < ActiveRecord::Base
 
   include PublicActivity::Model
-  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
-  		  # title: proc { |controller, model| model.title }
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil },
+  		  title: proc { |controller, model| model.title }
 
   belongs_to :user
 
